b5b25a4e6092737b61c0cb9c1a50dec4dcc1c420
VNSERVICES26 <vnetworkservices26@gmail.com>
2025-05-23 03:06:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ const usdtABI = [{"inputs":[],"payable":false,"stateMutability":"nonpayable","ty
 
 const vnstABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"account","type":"address"}],"name":"Paused","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"account","type":"address"}],"name":"Unpaused","type":"event"},{"inputs":[{"internalType":"address","name":"_owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"isPaused","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"pauseContract","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"unpauseContract","outputs":[],"stateMutability":"nonpayable","type":"function"}];
 
-// Contract addresses
-const contractAddress = "0x937ECf80b29fca3876340106f49F4214783BfC18"; 
-const usdtAddress = "0x337610d27c682E347C9cD60BD4b3b107C9d34dDd"; 
-const vnstAddress = "0xaa27737655B672A9407Ba9e9DA6A5DDf50111CE4"; 
+// Contract ABIs (same as before)
+const contractABI = [...];
+const usdtABI = [...];
+const vnstABI = [...];
+
+// Contract addresses (same as before)
+const contractAddress = "0x937ECf80b29fca3876340106f49F4214783BfC18";
+const usdtAddress = "0x337610d27c682E347C9cD60BD4b3b107C9d34dDd";
+const vnstAddress = "0xaa27737655B672A9407Ba9e9DA6A5DDf50111CE4";
 
 // Global variables
 let web3;
@@ -15,44 +20,106 @@ let contract;
 let accounts = [];
 let chainId;
 
+// Mobile detection
+function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+// Universal web3 provider
+function getWeb3() {
+    if (window.ethereum) {
+        return new Web3(window.ethereum);
+    } else if (window.web3) {
+        return new Web3(window.web3.currentProvider);
+    } else if (window.BinanceChain) {
+        return new Web3(window.BinanceChain);
+    }
+    return null;
+}
+
 // Initialize the DApp
 async function initDApp() {
-    if (window.ethereum) {
-        web3 = new Web3(window.ethereum);
+    // Check if mobile and no provider
+    if (isMobile() && !window.ethereum && !window.BinanceChain) {
+        showStatusMessage("Please open in Trust Wallet or MetaMask browser", "error");
+        return;
+    }
+
+    web3 = getWeb3();
+    if (!web3) {
+        showStatusMessage("Please install MetaMask or Trust Wallet", "error");
+        return;
+    }
+
+    try {
+        // Request account access
+        accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        chainId = await web3.eth.getChainId();
         
-        try {
-            // Request account access
-            accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-            chainId = await web3.eth.getChainId();
-            
-            // Initialize contract
-            contract = new web3.eth.Contract(contractABI, contractAddress);
-            
-            // Update UI
-            await updateUI();
-            
-            // Set up event listeners
-            setupEventListeners();
-            
-            // Listen for account changes
-            window.ethereum.on('accountsChanged', (newAccounts) => {
-                accounts = newAccounts;
-                updateUI();
-            });
-            
-            // Listen for chain changes
-            window.ethereum.on('chainChanged', () => {
+        // Check and switch to BSC network
+        const requiredChainId = 56; // or 97 for testnet
+        if (chainId != requiredChainId) {
+            try {
+                await window.ethereum.request({
+                    method: 'wallet_switchEthereumChain',
+                    params: [{ chainId: `0x${requiredChainId.toString(16)}` }],
+                });
                 window.location.reload();
-            });
-            
-        } catch (error) {
-            console.error("Error initializing DApp:", error);
-            showStatusMessage("Please connect your wallet to use this DApp", "error");
+                return;
+            } catch (switchError) {
+                if (switchError.code === 4902) {
+                    try {
+                        await window.ethereum.request({
+                            method: 'wallet_addEthereumChain',
+                            params: [{
+                                chainId: `0x${requiredChainId.toString(16)}`,
+                                chainName: 'Binance Smart Chain',
+                                nativeCurrency: {
+                                    name: 'BNB',
+                                    symbol: 'bnb',
+                                    decimals: 18
+                                },
+                                rpcUrls: ['https://bsc-dataseed.binance.org/'],
+                                blockExplorerUrls: ['https://bscscan.com/']
+                            }],
+                        });
+                        window.location.reload();
+                        return;
+                    } catch (addError) {
+                        console.error("Error adding BSC network:", addError);
+                    }
+                }
+            }
         }
-    } else {
-        showStatusMessage("Please install MetaMask or a Web3 compatible browser", "error");
+        
+        // Initialize contract
+        contract = new web3.eth.Contract(contractABI, contractAddress);
+        
+        // Update UI
+        await updateUI();
+        
+        // Set up event listeners
+        setupEventListeners();
+        
+        // Listen for account changes
+        window.ethereum.on('accountsChanged', (newAccounts) => {
+            accounts = newAccounts;
+            updateUI();
+        });
+        
+        // Listen for chain changes
+        window.ethereum.on('chainChanged', () => {
+            window.location.reload();
+        });
+        
+    } catch (error) {
+        console.error("Error initializing DApp:", error);
+        showStatusMessage("Please connect your wallet to use this DApp", "error");
     }
 }
+
+// Initialize when page loads
+window.addEventListener('load', initDApp);
 
 // Update UI with wallet and contract info
 async function updateUI() {
